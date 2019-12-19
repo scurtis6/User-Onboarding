3d828dc35ddadd690f866da61db8b02ad7d22371
Sierra Curtis <sierracurtis91@gmail.com>
2019-12-19 17:46:20 -0500

    added a post request

--- README.md
+++ README.md
@@ -5,12 +5,14 @@ We've seen many different styles of form management by now -- simple to complex.
 
 ## Set Up The Project
 
-- [ ] Start off by installing a blank React app by using Create React App.
-- [ ] Add the following as dependencies inside your React app:
-  - `formik`
-  - `yup`
-  - `axios`
-- [ ] Create a component file called `Form.js`, import it into your `App.js` file, and place the component in your JSX there.
+- [x] Start off by installing a blank React app by using Create React App.
+- [x] Add the following as dependencies inside your React app:
+  - `formik` yarn add formik
+  - `yup` yarn add yup
+  - `axios` yarn add axios
+  or yarn add formik yup axios
+  
+- [x] Create a component file called `Form.js`, import it into your `App.js` file, and place the component in your JSX there.
 
 ## STEP 1 - Create Your Formik Form
 
---
+++ my-app/src/components/Form.js
@@ -0,0 +1,114 @@
+import React, {useState, useEffect} from 'react';
+import {withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const Forms = ({
+    values,
+    errors,
+    touched,
+    status
+}) =>{
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        console.log(values);
+        console.log('status has changed', status);
+        status && setUsers(users => [...users, status]);
+    }, [status]);
+    
+    return (
+        <div className="user-form">
+            <Form>
+                <label htmlFor="name">Name:</label>
+                <Field 
+                id="name"
+                type="text"
+                name="name"
+                placeholder="name"
+                />
+                {touched.name && errors.name && (
+                    <p className="errors">{errors.name}</p>
+                )}
+                <label htmlFor="email">Email:</label>
+                <Field 
+                id="email"
+                type="text"
+                name="email"
+                placeholder="email"
+                />
+                {touched.email && errors.email && (
+                    <p className="errors">{errors.email}</p>
+                )}
+                <label htmlFor="password"> Password:</label>
+                <Field 
+                id="password"
+                type="text"
+                name="password"
+                placeholder="password"
+                />
+                {touched.password && errors.password && (
+                    <p className="errors">{errors.password}</p>
+                )}
+                <label htmlFor="terms" className="checkbox-container">
+                    Terms of Service
+                <Field
+                id="terms"
+                type="checkbox"
+                name="terms"
+                checked={values.terms} 
+                />
+                <span className="checkbox" />
+                </label>
+                <button type="submit">
+                    Enter
+                </button>
+            </Form>
+            {users.map(user => (
+                <ul key ={user.id}>
+                    <li>Name: {user.name}</li>
+                    <li>Email: {user.email}</li>
+                    <li>Password: *******</li>
+                </ul>
+            ))}
+        </div>
+    )
+    
+}
+const FormikForms = withFormik ({
+    mapPropsToValues({
+        name,
+        email,
+        password,
+        terms
+    }) {
+        return {
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            terms: terms || false
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required(
+            "This is a new error"
+        ),
+        email: Yup.string().required()
+    }),
+    handleSubmit(
+        values,
+        { setStatus, resetForm}
+     ) {
+     console.log("submitting", values);
+     axios
+     .post("https://reqres.in/api/users", values)
+     .then(res => {
+         console.log("succes", res);
+         setStatus(res.data);
+         resetForm();
+     })
+     .catch(err => 
+        console.log(err.response));
+ }    
+})(Forms);
+
+export default FormikForms;
